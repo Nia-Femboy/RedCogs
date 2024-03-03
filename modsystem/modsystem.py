63a9a44c1434e5622eb5c4066976d684cc536f60
WaterJump73 <privat@noktron.de>
2024-03-03 20:20:00 +0100

    Minor bugfix

--- modsystem/modsystem.py
+++ modsystem/modsystem.py
@@ -787,7 +787,7 @@ class Modsystem(commands.Cog):
             for index, userID in enumerate(dict(await self.config.guild(interaction.guild).userWarns()), start=1):
                 if(interaction.guild.get_member(userID) is None):
                     listString += (f"{await self.config.guild(interaction.guild).userWarns.get_raw(userID, 'displayName')}\n\n"
-                               f"Username: **{interaction.guild.get_member(int(userID)).name}**\n"
+                               f"Username: **{await self.config.guild(interaction.guild).userWarns.get_raw(userID, 'username')}**\n"
                                f"Aktuelle Points: **{await self.config.guild(interaction.guild).userWarns.get_raw(userID, 'currentPoints')}**\n"
                                f"Gesamte Points: **{await self.config.guild(interaction.guild).userWarns.get_raw(userID, 'totalPoints')}**\n"
                                f"Anzahl der Kicks: **{await self.config.guild(interaction.guild).userWarns.get_raw(userID, 'kickCount')}**\n")
@@ -797,7 +797,7 @@ class Modsystem(commands.Cog):
                                 f"Aktuelle Points: **{await self.config.guild(interaction.guild).userWarns.get_raw(userID, 'currentPoints')}**\n"
                                 f"Gesamte Points: **{await self.config.guild(interaction.guild).userWarns.get_raw(userID, 'totalPoints')}**\n"
                                 f"Anzahl der Kicks: **{await self.config.guild(interaction.guild).userWarns.get_raw(userID, 'kickCount')}**\n")
-                if(await self.config.guild(interaction.guild).userWWarns.get_raw(userID, 'banned') == False):
+                if(await self.config.guild(interaction.guild).userWarns.get_raw(userID, 'banned') == False):
                     listString += f"Verbleibende Punkte bis zum Ban: **{await self.config.guild(interaction.guild).warnBanWeight() - await self.config.guild(interaction.guild).userWarns.get_raw(userID, 'currentPoints')}**\n\n\n"
                 else:
                     listString += f"**User ist gebannt**\n\n\n"
